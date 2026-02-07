772647b5f32c48a62b259d1c904bad26230fb17a
Hamza <ghafarhamzaamsn.777@gmail.com>
2026-02-07 12:55:45 +0300

    Organize files && create home view

---
+++ lib/Features/Home/Presentation/Views/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/Features/Home/Presentation/Views/Widgets/home_view_body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeViewBody(),
+    );
+  }
+}
